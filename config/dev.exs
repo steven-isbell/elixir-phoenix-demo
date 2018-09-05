da00326321ed3ce8016a8fdd9e07d202bc25ddff
Steven Isbell <steveni_92@msn.com>
2018-09-04 23:39:50 -0500

    progress

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :countdown, Countdown.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "countdown_dev",
+  username: "stevenisbell",
+  password: "",
+  database: "elixer_demo",
   hostname: "localhost",
   pool_size: 10
